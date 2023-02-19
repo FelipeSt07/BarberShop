3637cba37d17af8be89c612a5bb9b10a2ff682e2
FelipeSt07 <felipesantacruz-2011@hotmail.com>
2023-02-18 23:37:03 -0500

    JH

--- prueba/productos.php
+++ prueba/productos.php
@@ -1,72 +1,78 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Tienda de productos</title>
-    <style>
-      /* Estilos CSS para la cuadrícula de productos */
-      .product-grid {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 20px;
-      }
-      
-      /* Estilos CSS para cada producto */
-      .product {
-        text-align: center;
-        border: 1px solid #ddd;
-        padding: 10px;
-      }
-      
-      /* Estilos CSS para la imagen del producto */
-      .product img {
-        max-width: 100%;
-        height: auto;
-      }
-      
-      /* Estilos CSS para el título del producto */
-      .product h2 {
-        margin-top: 0;
-        font-size: 1.2rem;
-      }
-      
-      /* Estilos CSS para la descripción del producto */
-      .product p {
-        font-size: 1rem;
-      }
-      
-      /* Estilos CSS para el precio del producto */
-      .product .price {
-        font-size: 1.2rem;
-        font-weight: bold;
-        color: green;
-        margin-top: 10px;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Tienda de productos</h1>
-    
-    <div class="product-grid">
-      <div class="product">
-        <img src="producto1.jpg" alt="Producto 1">
-        <h2>Producto 1</h2>
-        <p>Descripción del producto 1.</p>
-        <div class="price">$100.00</div>
-      </div>
-      <div class="product">
-        <img src="producto2.jpg" alt="Producto 2">
-        <h2>Producto 2</h2>
-        <p>Descripción del producto 2.</p>
-        <div class="price">$200.00</div>
-      </div>
-      <div class="product">
-        <img src="producto3.jpg" alt="Producto 3">
-        <h2>Producto 3</h2>
-        <p>Descripción del producto 3.</p>
-        <div class="price">$300.00</div>
-      </div>
-      <!-- Agrega más productos aquí -->
+
+<head>
+  <meta charset="UTF-8">
+  <title>Tienda de productos</title>
+  <div>
+    <h1>QUE BENDICION</h1>
+  </div>
+  <style>
+    /* Estilos CSS para la cuadrícula de productos */
+    .product-grid {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 20px;
+    }
+
+    /* Estilos CSS para cada producto */
+    .product {
+      text-align: center;
+      border: 1px solid #ddd;
+      padding: 10px;
+    }
+
+    /* Estilos CSS para la imagen del producto */
+    .product img {
+      max-width: 100%;
+      height: auto;
+    }
+
+    /* Estilos CSS para el título del producto */
+    .product h2 {
+      margin-top: 0;
+      font-size: 1.2rem;
+    }
+
+    /* Estilos CSS para la descripción del producto */
+    .product p {
+      font-size: 1rem;
+    }
+
+    /* Estilos CSS para el precio del producto */
+    .product .price {
+      font-size: 1.2rem;
+      font-weight: bold;
+      color: green;
+      margin-top: 10px;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Tienda de productos</h1>
+
+  <div class="product-grid">
+    <div class="product">
+      <img src="producto1.jpg" alt="Producto 1">
+      <h2>Producto 1</h2>
+      <p>Descripción del producto 1.</p>
+      <div class="price">$100.00</div>
     </div>
-  </body>
+    <div class="product">
+      <img src="producto2.jpg" alt="Producto 2">
+      <h2>Producto 2</h2>
+      <p>Descripción del producto 2.</p>
+      <div class="price">$200.00</div>
+    </div>
+    <div class="product">
+      <img src="producto3.jpg" alt="Producto 3">
+      <h2>Producto 3</h2>
+      <p>Descripción del producto 3.</p>
+      <div class="price">$300.00</div>
+    </div>
+    <!-- Agrega más productos aquí -->
+  </div>
+</body>
+
 </html>
