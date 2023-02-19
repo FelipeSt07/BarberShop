4d04ef62e2dbff57af1c650b84314a6514f5593e
FelipeSt07 <felipesantacruz-2011@hotmail.com>
2023-02-19 11:23:01 -0500

    domingo

--- prueba/productos.php
+++ prueba/productos.php
@@ -7,8 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="estilospro.css">
   <link rel="shortcut icon" href="imagenes/barber-shop.svg">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>Barberia Harlan</title>
 </head>
 
@@ -20,7 +21,7 @@
       <div class="left">
         <div class="btn-departamentos" id="btn-departamentos">
           <div class="boton">
-            <a href=""><i class="fa-solid fa-bars"></i></a>
+            <a id="oculto_b" href=""><i class="fa-solid fa-bars"></i></a>
           </div>
         </div>
 
@@ -30,7 +31,7 @@
       </div>
 
 
-      <div class="nav" id="nav_oculto">
+      <div class="nav_p" id="nav_p_oculto">
         <div class="div barra">
           <a href="login.php" id="oculto"><i class="fa-regular fa-circle-user"></i>Iniciar Sesion</a>
         </div>
@@ -46,51 +47,76 @@
     <div class="contenedor contenedor-grid">
       <div class="grid" id="grid">
         <div class="categorias">
-          <a href="#">Solo Hoy</a>
-          <a href="#">Solo para Ti</a>
-          <a href="#">Tiendas Oficiales</a>
-          <a href="#">Vende en Linio</a>
-          <a href="#">Ayuda</a>
-          <a href="#">Historial</a>
-          <a href="#">Banco Falabella</a>
-          <a href="#">Falabella</a>
-          <a href="#">Ventas Telefonicas</a>
+          <a id="oculto_a" href="#">Solo Hoy</a>
+          <a id="oculto_a" href="#">Solo para Ti</a>
+          <a id="oculto_a" href="#">Tiendas Oficiales</a>
+          <a id="oculto_a" href="#">Vende en Linio</a>
+          <a id="oculto_a" href="#">Ayuda</a>
+          <a id="oculto_a" href="#">Historial</a>
+          <a id="oculto_a" href="#">Banco Falabella</a>
+          <a id="oculto_a" href="#">Falabella</a>
+          <a id="oculto_a" href="#">Ventas Telefonicas</a>
         </div>
       </div>
     </div>
 
-    <main>
-      <div class="container2">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
+  </header>
+
+
+  <!--Contenido-->
+  <main>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="https://i.ibb.co/XCNWQHD/principal.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Zapatos color cafe</h5>
+              <p class="card-text">$ 599.00</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="#" class="btn btn-primary">Detalles</a>
                 </div>
+                <a href="#" class="btn btn-success">Agregar</a>
               </div>
             </div>
           </div>
         </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="https://i.ibb.co/XCNWQHD/principal.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Zapatos color cafe</h5>
+              <p class="card-text">$ 599.00</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="#" class="btn btn-primary">Detalles</a>
+                </div>
+                <a href="#" class="btn btn-success">Agregar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="https://i.ibb.co/XCNWQHD/principal.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Zapatos color cafe</h5>
+              <p class="card-text">$ 599.00</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="#" class="btn btn-primary">Detalles</a>
+                </div>
+                <a href="#" class="btn btn-success">Agregar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
+    </div>
+  </main>
 
-    </main>
-
-
-
-  </header>
 
   <footer>
     <section>
@@ -99,14 +125,16 @@
     <p>Copyright 2023</p>
   </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-    integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-    crossorigin="anonymous"></script>
+
   <script src="https://kit.fontawesome.com/7a4ffadb8c.js" crossorigin="anonymous"></script>
   <script src="main.js"></script>
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+
+
+
 </body>
 
 </html>
